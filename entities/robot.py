2aeafcc297b4828bf8cb6cad1027b141f341e837
AvidDavid23579 <aviddavid23579@gmail.com>
2026-07-06 20:48:55 +0700

    Added collision

--- entities/robot.py
+++ entities/robot.py
@@ -4,7 +4,7 @@ import numpy as np
 class Robot:
     # Differential drive robot parameters
     def __init__(self, x, y, theta, width=0.4, length=0.6):
-        self.x = x
+        self.x = x # Center
         self.y = y
         self.theta = theta
 
@@ -14,7 +14,32 @@ class Robot:
         self.v = 0.0
         self.omega = 0.0
 
+    def vertices(self):
+        hl = self.length / 2.0
+        hw = self.width / 2.0
+
+        # Corners in the robot's local frame (x forward, y left)
+        local_corners = [
+            (hl, hw),
+            (hl, -hw),
+            (-hl, -hw),
+            (-hl, hw),
+        ]
+
+        c, s = np.cos(self.theta), np.sin(self.theta)
+        world_corners = []
+        for lx, ly in local_corners:
+            wx = self.x + lx * c - ly * s
+            wy = self.y + lx * s + ly * c
+            world_corners.append((wx, wy))
+
+        return world_corners
+
     def update(self, dt):
         self.x += self.v * np.cos(self.theta) * dt
         self.y += self.v * np.sin(self.theta) * dt
         self.theta += self.omega * dt
+
+    def stop(self):
+        self.v = 0
+        self.omega = 0
--- world.py
+++ world.py
@@ -1,11 +1,48 @@
+from collision import sat_collision
+
+
 class World:
-    def __init__(self):
+    def __init__(self, bounds):
         self.robots = []
+        self.xmin, self.xmax, self.ymin, self.ymax = bounds
 
     def add_robot(self, robot):
         self.robots.append(robot)
 
+    def handle_collisions(self):
+        for robot_a, robot_b in self.robot_robot_collisions():
+            robot_a.stop()
+            robot_b.stop()
+
+        for robot in self.robot_wall_collisions():
+            robot.stop()
+    
+    def robot_robot_collisions(self):
+        collisions = []
+
+        for i in range(len(self.robots)):
+            for j in range(i + 1, len(self.robots)):
+                robot_a = self.robots[i]
+                robot_b = self.robots[j]
+
+                if sat_collision(robot_a.vertices(), robot_b.vertices()):
+                    collisions.append((robot_a, robot_b))
+            
+        return collisions
+    
+    def robot_wall_collisions(self):
+        collisions = []
+
+        for robot in self.robots:
+            for x, y in robot.vertices():
+                if x < self.xmin or x > self.xmax or y < self.ymin or y > self.ymax:
+                    collisions.append(robot)
+        
+        return collisions
+
     def step(self, dt):
+        self.handle_collisions()
+
         for robot in self.robots:
             robot.update(dt)
 
