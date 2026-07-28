32cacb0e0aa2687816b25e500fc72d1ce76fa011
AvidDavid23579 <aviddavid23579@gmail.com>
2026-07-28 11:20:14 +0700

    Added cost profiling

--- src/main.py
+++ src/main.py
@@ -1,3 +1,5 @@
+import cProfile
+import pstats
 import time
 
 from config import PHYSICS_DT
@@ -7,13 +9,15 @@ from simulator.world import World
 
 
 def main():
-    # Start simulation timer
+    # Start simulation timer and profiling process
+    profiler = cProfile.Profile()
+    profiler.enable()
     start = time.perf_counter()
 
     world = World()
     sim = Simulator(world)
 
-    duration = 10.0
+    duration = 10000.0
     steps = int(duration / PHYSICS_DT)
 
     sim.bake(steps)
@@ -21,6 +25,19 @@ def main():
     # End simulation timer
     elapsed = time.perf_counter() - start
     print(f"Simulation time: {elapsed:.6f} s")
+
+    # End profiling process
+    profiler.disable()
+
+    stats = pstats.Stats(profiler)
+
+    print("\n=== By cumulative time ===")
+    stats.sort_stats(pstats.SortKey.CUMULATIVE)  # Time including child calls
+    stats.print_stats(10)
+
+    print("\n=== By internal time ===")
+    stats.sort_stats(pstats.SortKey.TIME)  # Self time only
+    stats.print_stats(10)
 
     renderer = Renderer()
     renderer.playback(sim.timeline())
